6eba1b37d31a5e99996cb8625bf6fc6b7398b20c
Jeff Andrews <jeff.andrews@alphero.com>
2020-02-28 14:52:53 +1300

    Change browser websocket connection to work on any host

--- www-src/script.ts
+++ www-src/script.ts
@@ -11,7 +11,7 @@ const MESSAGE_CLASSES = {
 $(() => {
 
   const $console = $('#console');
-  const ws = new WebSocket('ws://localhost:8081');
+  const ws = new WebSocket(`ws://${location.hostname}:8081`);
 
   let userHasScrolledBack = false;
 
